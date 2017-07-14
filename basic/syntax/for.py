32ec9f0c0c334e935fe48395df0ea66b4d1c6bc7
Jiao Yongqi <jiaoyongqi@iton.cn>
2017-07-14 11:48:58 +0800

    for example

---
+++ basic/syntax/for.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+for  letter in 'Python':
+    print '当前字母:',letter
+
+fruits = ['banana','apple','mango']
+for fruit in fruits:
+    print '当前水果:',fruit
+print "Good bye!"
