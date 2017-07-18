3a3487cb22dd6153bb3f98dfb9fcafe9eb986c4a
jiaoyongqi <904771877@qq.com>
2017-07-18 17:54:52 +0800

    modify SMTP

--- basic/syntax/SMTP.py
+++ basic/syntax/SMTP.py
@@ -1,36 +1,5 @@
 # #!/usr/bin/python
 # # -*- coding: UTF-8 -*-
-#
-# import smtplib
-# from email.mime.text import MIMEText
-# from email.header import Header
-#
-# sender = 'we1583004we@163.com'
-# receivers = ['773163068@qq.com']
-#
-# server_host = 'smtp.163.com'
-#
-# username = 'we1583004we@163.com'
-# password = '@#we1238@#we'
-#
-# # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
-# # 三个参数：第一个为文本内容，第二个 plain 设置文本格式，第三个 utf-8 设置编码
-# message = MIMEText('Python 邮件发送测试...', 'plain', 'utf-8')
-# message['From'] = Header("菜鸟教程", 'utf-8')
-# message['To'] = Header("测试", 'utf-8')
-# subject = 'Python SMTP 邮件测试'
-# message['Subject'] = Header(subject, 'utf-8')
-# try:
-#      smtpObj = smtplib.SMTP()
-#      smtpObj.connect(server_host)
-#      smtpObj.login(username,password)
-#      smtpObj.sendmail(sender, receivers, message.as_string())
-#      smtpObj.close()
-#      print "邮件发送成功"
-# except smtplib.SMTPException:
-#      print "Error: 无法发送邮件"
-
-
 
 import smtplib
 from email.mime.text import MIMEText
@@ -64,7 +33,7 @@ def send(to_list, sub, content):
     print "发送成功"
   except Exception as e:
     print str(e)
-    print "发生失败"
+    print "发送失败"
 
 if __name__ == '__main__':
-  send(to_list, "这个是一个邮件", "<h1>Hello, It's test email.</h1>")
+  send(to_list, "这个是一个邮件", "<h1>Hello, It's  test email.</h1>")
