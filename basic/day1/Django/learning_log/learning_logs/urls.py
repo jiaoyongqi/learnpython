82edaf5d44ef36249a29fcaef1a5c66e4623568e
jiaoyongqi <904771877@qq.com>
2017-08-03 14:36:53 +0800

    modify bug

--- basic/day1/Django/learning_log/learning_logs/urls.py
+++ basic/day1/Django/learning_log/learning_logs/urls.py
@@ -5,10 +5,10 @@
 from django.conf.urls import url
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     #主页
     url(r'^$',views.index,name='index'),
 
     #显示所有的主题
     # url(r'^topics/$',views.topics,name='topics'),
-}
+]
