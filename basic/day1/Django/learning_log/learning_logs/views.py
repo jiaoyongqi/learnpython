81619d8183bea7e6e5d4b2e78740b4397c8c3da5
jiaoyongqi <904771877@qq.com>
2017-08-04 13:16:12 +0800

    增加添加新条目和编辑条目功能，目前添加新条目有bug

--- basic/day1/Django/learning_log/learning_logs/views.py
+++ basic/day1/Django/learning_log/learning_logs/views.py
@@ -4,8 +4,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
-from .models import Topic
-from .forms import TopicForm
+from .models import Topic,Entry
+from .forms import TopicForm,EntryForm
 
 # Create your views here.
 
@@ -40,3 +40,40 @@ def new_topic(request):
 
     context = {'form':form}
     return render(request,'learning_logs/new_topic.html',context)
+
+def new_entry(request,topic_id):
+    '''在待定的主题中添加新条目'''
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        #为提交数据,创建一个空表单
+        form = EntryForm()
+    else:
+        #POST提交的数据,对数据进行处理
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic',args=[topic.id]))
+
+        context = {'topic':topic,'form':form}
+        return render(request,'learning_logs/new_entry.html',context)
+
+def edit_entry(request,entry_id):
+    '''编辑既有条目'''
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        #初次请求,使用当前条目填充表单
+        form = EntryForm(instance=entry)
+    else:
+        #Post提交的数据,对数据进行处理
+        form = EntryForm(instance=entry,data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic',args=[topic.id]))
+
+    context = {'entry':entry,'topic':topic,'form':form}
+    return render(request,'learning_logs/edit_entry.html',context)
