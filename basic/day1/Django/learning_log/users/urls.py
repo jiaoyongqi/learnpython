01946b2973b0a2cb52ffdccf2dc490fd4336e856
jiaoyongqi <904771877@qq.com>
2017-08-07 11:34:32 +0800

    添加用户账户登录功能

---
+++ basic/day1/Django/learning_log/users/urls.py
@@ -0,0 +1,9 @@
+
+from django.conf.urls import url
+from django.contrib.auth.views import login
+
+from . import views
+
+urlpatterns = [
+    url(r'^login/$',login,{'template_name':'users/login.html'},name='login'),
+]
