48f16d83fccece566dae6f1b431e078834e2c193
jiaoyongqi <904771877@qq.com>
2017-08-07 11:51:52 +0800

    添加注销页面

--- basic/day1/Django/learning_log/users/views.py
+++ basic/day1/Django/learning_log/users/views.py
@@ -2,5 +2,13 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth import logout
 
 # Create your views here.
+
+def logout_view(request):
+    '''注销用户'''
+    logout(request)
+    return HttpResponseRedirect(reverse('learning_logs:index'))
