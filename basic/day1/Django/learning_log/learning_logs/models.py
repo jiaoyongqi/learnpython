38dcb57161d5720dc05cea922348c70a58de98b9
jiaoyongqi <904771877@qq.com>
2017-08-07 14:21:52 +0800

    让用户拥有自己的数据

--- basic/day1/Django/learning_log/learning_logs/models.py
+++ basic/day1/Django/learning_log/learning_logs/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 #在这里创建模型
@@ -10,6 +11,7 @@ class Topic(models.Model):
     '''用户学习的主题'''
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User)
 
     def __str__(self):
         '''返回模型的字符串表示'''
