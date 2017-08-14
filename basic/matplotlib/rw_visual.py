fae775db8c8b5d8948ad16e31546be498a923f15
jiaoyongqi <904771877@qq.com>
2017-08-14 11:47:55 +0800

    隐藏坐标轴

--- basic/matplotlib/rw_visual.py
+++ basic/matplotlib/rw_visual.py
@@ -13,6 +13,10 @@ while True:
     #突出起点和终点
     plt.scatter(0,0,c='green',edgecolors='none',s=100)
     plt.scatter(rw.x_values[-1],rw.y_values[-1],c='red',edgecolors='none',s=100)
+
+    #隐藏坐标轴
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
     plt.show()
 
     keep_running = raw_input("Make another walk ? (y/n): ")
