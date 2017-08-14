c5287dc2d7012f8c745e0e2afc490089707722d0
jiaoyongqi <904771877@qq.com>
2017-08-14 14:38:39 +0800

    添加最低气温绘制

--- basic/matplotlib/CSVdata/highs_lows.py
+++ basic/matplotlib/CSVdata/highs_lows.py
@@ -3,7 +3,7 @@ import csv
 from matplotlib import pyplot as plt
 from datetime import datetime
 
-#从文件中获取日期和最高气温
+#从文件中获取日期、最高气温和最低气温
 filename = 'sitka_weather_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
@@ -13,20 +13,24 @@ with open(filename) as f:
     #for index,column_header in enumerate(header_row):
     #   print(index,column_header)
 
-    dates,highs = [],[]
+    dates,highs,lows = [],[],[]
     for row in reader:
         current_date = datetime.strptime(row[0],"%Y-%m-%d")
         dates.append(current_date)
         high = int(row[1])
         highs.append(high)
 
+        low = int(row[3])
+        lows.append(low)
+
     print(highs)
 
     #根据数据绘制图形
     fig = plt.figure(dpi=128,figsize=(10,6))
     plt.plot(dates,highs,c='red')
+    plt.plot(dates,lows,c='blue')
     #设置图形的格式
-    plt.title("Daily high temperatures-2014",fontsize=24)
+    plt.title("Daily high and low temperatures-2014",fontsize=24)
     plt.xlabel('',fontsize=16)
     fig.autofmt_xdate()
     plt.ylabel("Temperature(F)",fontsize=16)
