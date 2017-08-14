d8c2477b31d0b1b1fe649e0f1a5dda9ed6f611ca
jiaoyongqi <904771877@qq.com>
2017-08-14 11:50:06 +0800

    增加点数

--- basic/matplotlib/rw_visual.py
+++ basic/matplotlib/rw_visual.py
@@ -4,11 +4,11 @@ from random_walk import RandomWalk
 
 while True:
     #创建一个RandomWalk实例,并将其包含的点都绘制出来
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
 
     point_numbers = list(range(rw.num_points))
-    plt.scatter(rw.x_values,rw.y_values,c=point_numbers,cmap=plt.cm.Blues,edgecolor='none',s=15)
+    plt.scatter(rw.x_values,rw.y_values,c=point_numbers,cmap=plt.cm.Blues,edgecolor='none',s=1)
 
     #突出起点和终点
     plt.scatter(0,0,c='green',edgecolors='none',s=100)
