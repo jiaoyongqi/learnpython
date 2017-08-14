3f808bfe4960401eb8e882ebb9fb4f5b7d31da6d
jiaoyongqi <904771877@qq.com>
2017-08-14 11:41:49 +0800

    绘制漫步图的样式

--- basic/matplotlib/rw_visual.py
+++ basic/matplotlib/rw_visual.py
@@ -6,7 +6,9 @@ while True:
     #创建一个RandomWalk实例,并将其包含的点都绘制出来
     rw = RandomWalk()
     rw.fill_walk()
-    plt.scatter(rw.x_values,rw.y_values,s=15)
+
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values,rw.y_values,c=point_numbers,cmap=plt.cm.Blues,edgecolor='none',s=15)
     plt.show()
 
     keep_running = raw_input("Make another walk ? (y/n): ")
