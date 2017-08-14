72a1069f1d92a1cc37d13e5ce3ad8fd4f3c04c9d
jiaoyongqi <904771877@qq.com>
2017-08-14 13:42:10 +0800

    同时掷两面不同的骰子

--- basic/matplotlib/Pygal/die_visual.py
+++ basic/matplotlib/Pygal/die_visual.py
@@ -3,13 +3,13 @@
 from die import Die
 import pygal
 
-#创建一个D6
+#创建一个D6和一个D10
 die_1 = Die()
-die_2 = Die()
+die_2 = Die(10)
 
 #掷几次骰子,并将结果存储在一个列表中
 results = []
-for roll_num in range(1000):
+for roll_num in range(50000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
 
@@ -25,10 +25,10 @@ for value in range(2,max_result+1):
 #对结果进行可视化
 hist = pygal.Bar()
 
-hist.title = "Results of rolling two D6 1000 times."
-hist.x_labels = ['2','3','4','5','6','7','8','9','10','11','12']
+hist.title = "Results of rolling a D6 and a D10 50000 times."
+hist.x_labels = ['2','3','4','5','6','7','8','9','10','11','12','13','14','15','16']
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
 
-hist.add('D6 + D6',frequencies)
-hist.render_to_file('die_visual2.svg')
+hist.add('D6 + D10',frequencies)
+hist.render_to_file('die_visual3.svg')
