b9336ca12bebc9feb42128e6efa20a1dc659adcf
jiaoyongqi <904771877@qq.com>
2017-08-14 14:46:32 +0800

    错误检查

--- basic/matplotlib/CSVdata/highs_lows.py
+++ basic/matplotlib/CSVdata/highs_lows.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 from datetime import datetime
 
 #从文件中获取日期、最高气温和最低气温
-filename = 'sitka_weather_2014.csv'
+filename = 'death_valley_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -15,15 +15,16 @@ with open(filename) as f:
 
     dates,highs,lows = [],[],[]
     for row in reader:
-        current_date = datetime.strptime(row[0],"%Y-%m-%d")
-        dates.append(current_date)
-        high = int(row[1])
-        highs.append(high)
-
-        low = int(row[3])
-        lows.append(low)
-
-    print(highs)
+        try:
+            current_date = datetime.strptime(row[0],"%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3])
+        except ValueError:
+            print(current_date,'missing data')
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
     #根据数据绘制图形
     fig = plt.figure(dpi=128,figsize=(10,6))
