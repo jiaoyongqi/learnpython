cc63525b22731e201d5629009f937284eef8cb38
jiaoyongqi <904771877@qq.com>
2017-08-14 13:03:05 +0800

    设置绘图窗口的尺寸

--- basic/matplotlib/rw_visual.py
+++ basic/matplotlib/rw_visual.py
@@ -7,6 +7,9 @@ while True:
     rw = RandomWalk(50000)
     rw.fill_walk()
 
+    #设置绘图窗口的尺寸
+    plt.figure(figsize=(10,6))
+
     point_numbers = list(range(rw.num_points))
     plt.scatter(rw.x_values,rw.y_values,c=point_numbers,cmap=plt.cm.Blues,edgecolor='none',s=1)
 
