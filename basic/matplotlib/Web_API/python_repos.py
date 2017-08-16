f5210ea727e04668579ea8d42a1aa031aeaeae8e
jiaoyongqi <904771877@qq.com>
2017-08-16 16:04:10 +0800

    改进Pygal图表

--- basic/matplotlib/Web_API/python_repos.py
+++ basic/matplotlib/Web_API/python_repos.py
@@ -22,9 +22,21 @@ for repo_dict in repo_dicts:
 
 #可视化
 my_style = LS('#333366',base_style=LCS)
-chart = pygal.Bar(style=my_style,x_label_rotation=45,show_legend=False)
+
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config,style=my_style)
+#chart = pygal.Bar(style=my_style,x_label_rotation=45,show_legend=False)
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
 
 chart.add('',stars)
-chart.render_to_file('python_repos.svg')
+chart.render_to_file('python_repos2.svg')
