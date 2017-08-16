c06759844d2e4ab8b1074954a9e75446e293010a
jiaoyongqi <904771877@qq.com>
2017-08-16 15:34:18 +0800

    使用Pygal可视化仓库

--- basic/matplotlib/Web_API/python_repos.py
+++ basic/matplotlib/Web_API/python_repos.py
@@ -1,5 +1,7 @@
 # -*-coding:UTF-8-*-
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS,LightenStyle as LS
 
 #执行API调用并存储相应
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -12,20 +14,17 @@ print ("Total repositories:",response_dict['total_count'])
 
 #探索有关仓库的信息
 repo_dicts = response_dict['items']
-print ("Repositories returned:",len(repo_dicts))
 
-#研究地一个仓库
-repo_dict = repo_dicts[0]
-# print("\n Keys:",len(repo_dict))
-# for key in sorted(repo_dict.keys()):
-#     print(key)
-#
+names,stars = [],[]
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
 
-print("\n Selected information about first repository:")
-print('Name:',repo_dict['name'])
-print('Owner:',repo_dict['owner']['login'])
-print('Stars:',repo_dict['stargazers_count'])
-print('Repository:',repo_dict['html_url'])
-print('Created:',repo_dict['created_at'])
-print('Updated:',repo_dict['updated_at'])
-print('Description:',repo_dict['description'])
+#可视化
+my_style = LS('#333366',base_style=LCS)
+chart = pygal.Bar(style=my_style,x_label_rotation=45,show_legend=False)
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = names
+
+chart.add('',stars)
+chart.render_to_file('python_repos.svg')
