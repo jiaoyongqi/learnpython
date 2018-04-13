d0e14013d9784a1a49141e1e1e8bda6034a4e389
jiaoyongqi <904771877@qq.com>
2018-04-13 17:55:52 +0800

    Thread子类MyThread

---
+++ basic/pythoncore/4/thread/MyThread.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import threading
+from time import sleep,ctime
+
+class MyThread(threading.Thread):
+    def __init__(self,func,args,name=''):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.func = func
+        self.args = args
+
+    def GetResult(self):
+        return self.res
+
+    def run(self):
+        print 'starting',self.name,'at:',ctime()
+        self.res = self.func(*self.args)
+        print self.name,'finished at:',ctime()
