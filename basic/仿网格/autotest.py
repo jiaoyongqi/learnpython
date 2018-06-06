e5dc4b8c0f197e3f2de30a3d7f8f3088744f5614
jiaoyongqi <904771877@qq.com>
2018-06-06 13:42:22 +0800

    添加雷达障碍物显示代码

---
+++ basic/仿网格/autotest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# coding: utf-8
+import webbrowser as web
+import os
+
+
+def run_to_use_default_browser_open_url(url):
+    web.open_new_tab(url)
+    print 'run_to_use_default_browser_open_url  open url ending ....'
+
+
+def browser_test():
+    os.system('chmod 777 writetext.txt')
+    os.system('python -m SimpleHTTPServer 8080 &')
+    url = 'http://0.0.0.0:8080'
+    run_to_use_default_browser_open_url(url)
+
+
+if __name__ == '__main__':
+    browser_test()
